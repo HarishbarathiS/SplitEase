17f8f62f79a1efe845fe8903bc3151d546c2a388
unknown <you@example.com>
2025-05-23 23:56:27 +0530

    update split logic for adding multiple expenses

--- backend/split_logic.py
+++ backend/split_logic.py
@@ -9,50 +9,42 @@ class Transaction:
 
 
 class ExpenseSplitter:
-    def __init__(self, total_members : int, items : dict, tax = 0):
-        self.total_members = total_members
-        self.items = items
-        self.payer_share_to_item = defaultdict(dict)
-        self.tax = tax
+    def __init__(self, members):
         self.payer_to_total_share = OrderedDict()
+        self.members = members
         self.contribution = OrderedDict()
-        self.members = []
     
-    def split_by_items(self,item_to_payers : defaultdict):
-        
-        tax_share = round(self.tax / self.total_members,2)
+    def split_by_items(self, item_to_payers : defaultdict, items : dict):
+        payer_share_to_item = defaultdict(dict)
         for item, payers in item_to_payers.items():
             number_of_payers = len(payers)
-            share = round(self.items[item] / number_of_payers,2)
+            share = round(items[item] / number_of_payers,2)
             for payer in payers:
-                self.payer_share_to_item[payer][item] = share
-        # print(self.payer_share_to_item)
+                payer_share_to_item[payer][item] = share
         
-        # total_amt = 0
-        for name, items in self.payer_share_to_item.items():
+        for name, items in payer_share_to_item.items():
             total_share = 0
             for _,share in items.items():
-                # total_amt += share
                 total_share += share
-            self.payer_to_total_share[name] = round(total_share + tax_share,2)
-        # print(self.payer_share_to_item)
-        # print(self.payer_to_total_share)
+            self.payer_to_total_share[name] = self.payer_to_total_share.get(name, 0.0) + round(total_share,2)
+        print(payer_share_to_item)
+        print(self.payer_to_total_share)
         
-    def calculateTransactions(self, contribution, members):
-        self.members = members
+    def calculateTransactions(self, contribution, tax):
         self.payer_to_total_share = OrderedDict(sorted(self.payer_to_total_share.items(), key=lambda x: x[1]))
-        self.contribution = OrderedDict(sorted(contribution.items(), key=lambda x: x[1]))
-        # print(self.contribution)
+        contribution = OrderedDict(sorted(contribution.items(), key=lambda x: x[1]))
+        
         
         net_balance = OrderedDict()
+        tax_share = round(tax / len(self.members), 2)
         
         for member in self.members:
             if member in self.payer_to_total_share.keys():
-                if member in self.contribution.keys():
-                    net_balance[member] = round(self.contribution[member] - self.payer_to_total_share[member],2)
+                if member in contribution.keys():
+                    self.contribution[member] = self.contribution.get(member, 0.0) + contribution[member]
+                    net_balance[member] = round(self.contribution[member] - self.payer_to_total_share[member] - tax_share,2)
                 else:
-                    net_balance[member] = -self.payer_to_total_share[member]
-        # print(net_balance)
+                    net_balance[member] = -self.payer_to_total_share[member] - tax_share
         
         net_balance = OrderedDict(sorted(net_balance.items(), key=lambda x: x[1]))   
         print(net_balance)
@@ -84,7 +76,6 @@ class ExpenseSplitter:
         
         for transaction in transactions:
             print(f"{transaction.payer} owes {transaction.receipent} {transaction.amt}")
-        
         
         
 # # Case 1 : 1 member pays the total_amt and equal split
@@ -131,17 +122,48 @@ class ExpenseSplitter:
 # obj.calculateTransactions(contribution, members)
 
 
-# # Case 4 : Multiple member pay the total_amt and unequal split
+# # # Case 4 : Multiple member pay the total_amt and unequal split
 
 
-items = {"pasta" : 200 , "pizza" : 350}
-item_to_payers = {"pizza" : {"harish","naveen","appa"}, "pasta" : {"appa","amma"}}
-tax = 97.8 
-total_members = 4
-contribution = OrderedDict({"harish" : 200, "naveen" : 100, "appa" : 250})
-members = ["harish","amma","appa","naveen"]
+# items = {"pasta" : 200 , "pizza" : 350}
+# item_to_payers = {"pizza" : {"harish","naveen","appa"}, "pasta" : {"appa","amma"}}
+# tax = 97.8 
+# total_members = 4
+# contribution = OrderedDict({"harish" : 200, "naveen" : 100, "appa" : 250})
+# members = ["harish","amma","appa","naveen"]
+
+
+# Expense 1
+
+# items = {"pasta" : 200 , "pizza" : 350}
+# item_to_payers = {"pizza" : {"harish","naveen","appa"}, "pasta" : {"appa","amma"}}
+# tax = 97.8 
+# total_members = 4
+# contribution = OrderedDict({"harish" : 200, "naveen" : 100, "appa" : 250})
+# members = ["harish","amma","appa","naveen"]
+
+# Expense 2
+
+items = {"pasta" : 200 , "pizza" : 300}
+item_to_payers = {"pizza" : {"harish","naveen","krizna","surith"}, "pasta" : {"harish","naveen","krizna","surith"}}
+tax = 0
+contribution = OrderedDict({"harish" : 320, "naveen" : 180})
+members = ["harish","naveen","krizna","surith"]
+
+
+
 
         
-obj = ExpenseSplitter(total_members,items,tax)
-obj.split_by_items(item_to_payers)
-obj.calculateTransactions(contribution, members)
+obj = ExpenseSplitter(members)
+obj.split_by_items(item_to_payers, items)
+obj.calculateTransactions(contribution, tax)
+
+items = {"pasta" : 350 , "pizza": 650}
+item_to_payers = {"pizza" : {"harish","naveen","krizna","surith"}, "pasta" : {"harish","naveen","krizna","surith"}}
+tax = 0
+contribution = OrderedDict({"harish" : 400, "naveen" : 600})
+members = ["harish","naveen","krizna","surith"]
+
+
+obj.split_by_items(item_to_payers, items)
+obj.calculateTransactions(contribution, tax)
